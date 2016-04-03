8f614c84387117b5c1fae739231226caa735dac2
Mitch Kroska <kroskam8457@hotmail.com>
2016-04-03 10:42:21 -0500

    Add User/Attendee assosiations

---
+++ db/migrate/20160403103024_create_attendees.rb
@@ -0,0 +1,9 @@
+class CreateAttendees < ActiveRecord::Migration
+  def change
+    create_table :attendees do |t|
+      t.integer :user_id
+
+      t.timestamps(null:false)
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,8 @@ require 'faker'
 100.times do 
   User.create(name: Faker::Name.name)
 end
+
+50.times do 
+  user = (1..100).to_a.sample
+  Attendee.create(user_id: user)
+end
